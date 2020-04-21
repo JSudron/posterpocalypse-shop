0873210368bd932eb6b22b7ba609220dd362bf27
Joe Sudron <joesudron@outlook.com>
2020-04-21 12:06:34 +0000

    created the accounts app & copied over the accounts py files from previous project

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from accounts.views import logout, login, register, profile
+from accounts import urls_reset
+
+
+urlpatterns = [
+    url(r'^logout/', logout, name="logout"),
+    url(r'^login/', login, name="login"),
+    url(r'^register/', register, name="register"),
+    url(r'^profile/', profile, name="profile"),
+    url(r'^password-reset/', include(urls_reset))
+]
---
+++ accounts/views.py
@@ -0,0 +1,104 @@
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from accounts.forms import UserLoginForm, UserRegistrationForm
+import sweetify
+
+
+def register(request):
+    """
+    Render registration page
+    """
+    if request.user.is_authenticated:
+        sweetify.info(request, "You're already logged in",
+                         button='Ok', timer=3000)
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request)
+                sweetify.success(request,
+                                 "You have successfully registered your account")
+                return redirect(reverse('index'))
+            else:
+                sweetify.info(request,
+                              "Sorry but we're unable to register your account at this time!",
+                              button='Ok', timer=3000)
+    else:
+        # Create an instance of reg form
+        registration_form = UserRegistrationForm()
+
+    return render(request, 'registration.html', {
+        "registration_form": registration_form})
+    """
+    registration_form passed through as a context dictionary.
+    """
+
+
+def login(request):
+    """
+    Render login page
+    """
+    if request.user.is_authenticated:
+        sweetify.info(request, "You're already logged in!",
+                         button='OK', timer=3000)
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+
+            if user:
+                auth.login(user=user, request=request)
+                sweetify.success(request,
+                                 "You have successfully logged in!")
+
+                if request.GET.get('next', False):
+                    return HttpResponseRedirect(request.GET.get('next'))
+                else:
+                    return redirect(reverse('index'))
+            else:
+                login_form.add_error(None,
+                                     "Your username or password is incorrect!")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {"login_form": login_form})
+
+
+@login_required
+def logout(request):
+    """
+    Logout user
+    """
+    auth.logout(request)
+    sweetify.success(request, "You have successfully been logged out!")
+
+    return redirect(reverse('index'))
+
+
+@login_required()
+def profile(request):
+    """
+    User profile page. Filter () used to return multiple records and help
+    avoid an error message if using get () instead. The latter method is
+    limited to getting one result only.
+    """
+    user = User.objects.filter(email=request.user.email)
+
+    context = {
+        'profile': user,
+    }
+
+    return render(request, 'profile.html', context)
